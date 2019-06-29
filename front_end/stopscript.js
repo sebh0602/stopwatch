63945d846255e20473148dd3694ca08fea29f814
Sebastian Hietsch <hietschseb@outlook.com>
2019-06-29 13:10:11 +0200

    Finish local part except log editing

--- front_end/stopscript.js
+++ front_end/stopscript.js
@@ -29,6 +29,10 @@ function controlHandler(type){
 				localStorage.removeItem("log");
 				document.getElementById("log").value = "";
 			}
+			document.getElementById("hours").innerHTML = "00";
+			document.getElementById("minutes").innerHTML = "00";
+			document.getElementById("seconds").innerHTML = "00";
+			document.getElementById("fracs").innerHTML = "0";
 			break;
 		default:
 			if (localStorage.log == undefined){
@@ -46,17 +50,55 @@ function updateTime(){
 	}
 	var splitLog = log.split("\n");
 	splitLog = splitLog.slice(0,splitLog.length - 1);
-	if (splitLog[splitLog.length - 1].includes("start")){
-		var lastStart = splitLog[splitLog.length - 1].split("|")[1];
-		var delta = new Date() - new Date(lastStart);
-		console.log(delta);
+	var nextCommand = "start";
+	var start;
+	var stop;
+	var totalDelta = 0;
+	for (var i = 0; i < splitLog.length; i++){
+		if (splitLog[i].includes(nextCommand)){
+			if (nextCommand == "start"){
+				start = new Date(splitLog[i].split("|")[1]);
+				nextCommand = "stop";
+			} else{
+				stop = new Date(splitLog[i].split("|")[1]);
+				totalDelta += stop - start;
+				nextCommand = "start";
+			}
+		}
 	}
-	/*var t = new Date()
-	document.getElementById("hours").innerHTML = t.getHours();
-	document.getElementById("minutes").innerHTML = t.getMinutes();
-	document.getElementById("seconds").innerHTML = t.getSeconds();
-	//document.getElementById("fracs").innerHTML = Math.floor(t.getMilliseconds()/10);*/
+	if (nextCommand == "stop"){
+		totalDelta += new Date() - start;
+	}
+
+	var hours = Math.floor(totalDelta / 3600000);
+	totalDelta -= hours * 3600000;
+	hours = hours.toString();
+	if (hours.length < 2){
+		hours = "0" + hours;
+	}
+
+	var minutes = Math.floor(totalDelta / 60000);
+	totalDelta -= minutes * 60000;
+	minutes = minutes.toString();
+	if (minutes.length < 2){
+		minutes = "0" + minutes;
+	}
+
+	var seconds = Math.floor(totalDelta / 1000);
+	totalDelta -= seconds * 1000;
+	seconds = seconds.toString();
+	if (seconds.length < 2){
+		seconds = "0" + seconds;
+	}
+
+	var fracs = Math.floor(totalDelta / 100);
+
+
+	document.getElementById("hours").innerHTML = hours;
+	document.getElementById("minutes").innerHTML = minutes;
+	document.getElementById("seconds").innerHTML = seconds;
+	document.getElementById("fracs").innerHTML = fracs;
 }
 
 run();
-var iv = setInterval(updateTime, 200);
+var iv = setInterval(updateTime, 100);
